048c31a98b1615c96b315d92501983a53f40ae76
Waqar Ahmed <waqarsonic1@gmail.com>
2018-08-30 12:14:45 +0500

    Retrive Active VCPU count for VM
    This commit introduces the ability for the end user to find active vcpu count for a specified VM. Many thanks to @araujobsd for his help in making this work and walking me through the internals of bhyve.
    Following functionalites have been introduced:
    1) get_stats function has been improved which now retrieves stats for all active vcpus of a vm by default and if only stats are required for a specified vm, that can be specified as well.
    2) active_vcpus has been introduced which will return a count of active vcpu for the specified vm.

--- bhyve.pyx
+++ bhyve.pyx
@@ -1,4 +1,5 @@
-cimport bhyve
+cimport vmmapi
+cimport cpuset
 cimport types
 cimport vmm
 
@@ -12,6 +13,7 @@ from libc.stdlib cimport free
 class Error(enum.IntEnum):
     OPEN_FAILED = 100
     STATS_FETCH_FAILED = 101
+    RETRIEVE_ACTIVE_VCPU_FAILED = 102
 
 
 class BhyveException(RuntimeError):
@@ -22,16 +24,16 @@ class BhyveException(RuntimeError):
 cdef class VM:
 
     cdef str vm_name
-    cdef bhyve.vmctx* vm
+    cdef vmmapi.vmctx *vm
 
     def __cinit__(self, str vm_name_p, bint create_vm=0):
         self.vm_name = vm_name_p
         vm_name_bytes = vm_name_p.encode()
-        cdef char* vm_name = vm_name_bytes
+        cdef char *vm_name = vm_name_bytes
 
         if create_vm:
             with nogil:
-                code = bhyve.vm_create(vm_name)
+                code = vmmapi.vm_create(vm_name)
 
             if code != 0:
                 raise BhyveException(
@@ -39,7 +41,7 @@ cdef class VM:
                 )
 
         with nogil:
-            self.vm = bhyve.vm_open(vm_name)
+            self.vm = vmmapi.vm_open(vm_name)
 
         if self.vm == NULL:
             raise BhyveException(
@@ -48,7 +50,7 @@ cdef class VM:
 
     def force_reset(self):
         with nogil:
-            error = bhyve.vm_suspend(self.vm, vmm.VM_SUSPEND_RESET)
+            error = vmmapi.vm_suspend(self.vm, vmm.VM_SUSPEND_RESET)
 
         if error:
             raise BhyveException(
@@ -59,7 +61,7 @@ cdef class VM:
 
     def force_poweroff(self):
         with nogil:
-            error = bhyve.vm_suspend(self.vm, vmm.VM_SUSPEND_POWEROFF)
+            error = vmmapi.vm_suspend(self.vm, vmm.VM_SUSPEND_POWEROFF)
 
         if error:
             raise BhyveException(
@@ -68,27 +70,39 @@ cdef class VM:
 
         return True
 
-    def get_stats(self, int vcpu=0):
-        # TODO: Modify this to support stats for all vcpus
-        if not -1 < vcpu < 16:
+    def get_stats(self, vcpu=None):
+        active_vcpus = self.active_vcpus()
+        if vcpu is not None and not -1 < vcpu < active_vcpus:
             raise BhyveException(
-                Error.STATS_FETCH_FAILED, 'Please specify a vcpu number from 1-16'
+                Error.STATS_FETCH_FAILED, f'Please specify a vcpu number from 0-{active_vcpus}'
             )
 
-        cdef bhyve.timeval tv
+        stats = {}
+        if vcpu is None:
+            for i in range(active_vcpus):
+                stats[f'cpu_{i}'] = self.__get_stats(i)
+        else:
+            stats[f'cpu_{vcpu}'] = self.__get_stats(vcpu)
+
+        return stats
+
+    def __get_stats(self, int vcpu):
+        # TODO: Should we be using vcpu count from 1-16 or 0-15 ? There are 2 perspectives to this
+        # End user will expect something from 1 -16 and a dev from 0 - 15 as the underlying api uses this
+        cdef vmmapi.timeval tv
         cdef int num_stats, i = 0
         cdef types.uint64_t *stats
         cdef const char *desc
 
         with nogil:
-            stats = bhyve.vm_get_stats(self.vm, vcpu, &tv, &num_stats)
+            stats = vmmapi.vm_get_stats(self.vm, vcpu, &tv, &num_stats)
 
         stat_list = []
 
         if stats != NULL:
             while i < num_stats:
                 stat_list.append({
-                    'description': bhyve.vm_get_stat_desc(self.vm, i).decode(),
+                    'description': vmmapi.vm_get_stat_desc(self.vm, i).decode(),
                     'value': stats[i]
                 })
                 i += 1
@@ -101,7 +115,20 @@ cdef class VM:
 
     def destroy(self):
         with nogil:
-            bhyve.vm_destroy(self.vm)
+            vmmapi.vm_destroy(self.vm)
+
+    def active_vcpus(self):
+        cdef cpuset.cpuset_t cpus
+
+        with nogil:
+            error = vmmapi.vm_active_cpus(self.vm, &cpus)
+
+        if not error:
+            return [True for i in range(vmm.VM_MAXCPU) if cpuset.CPU_ISSET(i, &cpus)].count(True)
+        else:
+            raise BhyveException(
+                Error.RETRIEVE_ACTIVE_VCPU_FAILED, 'Failed to retrieve active vcpu count'
+            )
 
     def __dealloc__(self):
         free(self.vm)
--- pxd/vmmapi.pxd
+++ pxd/vmmapi.pxd
@@ -1,3 +1,4 @@
+cimport cpuset
 cimport types
 cimport vmm
 
@@ -11,7 +12,8 @@ cdef extern from 'vmmapi.h' nogil:
 
     extern int vm_create(const char *name)
     extern vmctx *vm_open(const char *name)
-    extern void	vm_destroy(vmctx *ctx)
+    extern void vm_destroy(vmctx *ctx)
     extern types.uint64_t *vm_get_stats(vmctx *ctx, int vcpu, timeval *ret_tv, int *ret_entries)
     extern const char *vm_get_stat_desc(vmctx *ctx, int index)
     extern int vm_suspend(vmctx *ctx, vmm.vm_suspend_how how)
+    extern int vm_active_cpus(vmctx *ctx, cpuset.cpuset_t *cpus)
